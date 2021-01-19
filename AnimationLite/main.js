dc390fc7b47b89054e9788986cbf1666aef27bb0
KenpachiZ11 <mr.daniil.b@mail.ru>
2021-01-19 19:18:15 +0300

    AnimationLite
    VersionOne

---
+++ AnimationLite/main.js
@@ -0,0 +1,11 @@
+  let cat = document.querySelector("img");
+  let angle = 0, lastTime = null;
+  function animate(time) {
+    if (lastTime != null)
+      angle += (time - lastTime) * 0.001;
+    lastTime = time;
+    cat.style.top = (Math.sin(angle) * 20) + "px";
+    cat.style.left = (Math.cos(angle) * 200) + "px";
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
